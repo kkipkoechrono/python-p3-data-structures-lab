ab64dbadacf31e3f9666ff5d7102ad3d35992016
ykentrell545@gmail.com <xokentr3ll@DESKTOP-4BEPFC0>
2024-09-24 11:29:37 -0700

    lists the last two functions

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -25,7 +25,7 @@ def get_spiciest_foods(spicy_foods):
 def print_spicy_foods(spicy_foods):
     for food in spicy_foods:
         print(f"{food['name']} ({food['cuisine']}) | Heat Level: {'🌶' * food['heat_level']}")
-
+        
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     for food in spicy_foods:
         if food['cuisine'] == cuisine:
